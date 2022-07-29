cb215ce05f4794e848af76117762a83caddf4b3d
tienhoangg <hoangtrantien202@gmail.com>
2022-07-30 06:10:21 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,42 +1,25 @@
-import { userState, useState } from "react";
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
-import Homepage from "./pages/home/Homepage";
-import Login from "./pages/auth/login/login";
-import Register from "./pages/auth/register/register";
-import Verify from "./pages/auth/verify/verify";
-import ShowImage from "./pages/showImage/ShowImage";
-import Upload from "./pages/upload/Upload";
-import Profile from "./pages/profile/Profile";
-import ShowImageForCategory from "./pages/showImageForCategory/showImageForCategory";
-import AboutPage from "./pages/about/about";
-import ShowCategory from "./pages/showCategory/showCategory";
-import LeaderBoard from "./pages/leaderBoard/leaderBoard";
-import ResultSearch from "./pages/resultSearch/resultSearch";
-import "bootstrap/dist/css/bootstrap.min.css";
-import{toast, ToastContainer} from 'react-toastify'
-import "react-toastify/dist/ReactToastify.css"
+import logo from './logo.svg';
+import './App.css';
+
 function App() {
   return (
-    <>
-      <ToastContainer />
-      <Router>
-        <Routes>
-          <Route path="/home" element={<Homepage />} default/>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />}/>
-          <Route path="/verify/:token" element={<Verify />}/>
-          <Route path="/testShowImage" element={<ShowImage />}/>
-          <Route path="/upload" element={<Upload />}/>
-          <Route path="/profile" element={<Profile />}/>
-          <Route path="/leaderboards" element={<LeaderBoard />}/>
-          <Route path="/showcategory" element={<ShowCategory />}/>
-          <Route path="/showimageforcategory" element={<ShowImageForCategory/>}/>
-          <Route path="/about" element={<AboutPage />}/>
-          <Route path="/resultSearch" element={<ResultSearch />}/>
-          <Route path="*" element={<Navigate to="/home" replace />} />
-        </Routes>
-      </Router>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
+
 export default App;
