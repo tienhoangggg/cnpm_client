4628d95638b07ff18d9216e7bdc9310fbe6e939e
Sawashi <kietle234.cc@gmail.com>
2022-08-01 12:55:57 +0700

    Add form to comment

--- src/components/CommentSection.jsx
+++ src/components/CommentSection.jsx
@@ -8,12 +8,29 @@ import "./cssComponent/CommentSection.css";
 import { FaQuestion, FaUser, FaKey, FaListUl } from 'react-icons/fa'
 import React from 'react'
 function CommentSection() {
+    function CommentBox(props) {
+        const isLoggedIn = props.isLoggedIn;
+        if (isLoggedIn == 1) {
+            return(
+                <div style={{textAlign: "center"}}>
+                 <img src="https://drive.google.com/uc?export=view&id=1e7tzVJ9N8lllMXNPhvwX1GxxJpDRHoxs" id="avatarComment" />
+                 &nbsp;&nbsp;&nbsp;
+                 <input type="text" placeholder="Enter your comment" style={{width: "80%", height: "5vh", overflow: "scroll", borderRadius: "0%"}}/>
+                </div>
+            );
+        } else {
+          return (
+            <div style={{color: "white", textAlign: "center"}}>You need to login to leave a comment</div>
+            );
+        }
+    }
     const imageData = ['https://drive.google.com/uc?export=view&id=1e7tzVJ9N8lllMXNPhvwX1GxxJpDRHoxs', 'https://drive.google.com/uc?export=view&id=1e7tzVJ9N8lllMXNPhvwX1GxxJpDRHoxs']
     const nameData = ['Test1', 'Test1']
     const dateData = ['1/1/2022', '1/1/2022']
     const desData = ['This so goood', 'This so goood']
     return (
         <>
+            <CommentBox isLoggedIn={getCookie('logged')}/>
             <div style={{ padding: "2vh" }}>
                 {imageData.map((linkT, index) =>
                     <>
