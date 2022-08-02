de0f0a7fe7167dddae1e57df0884dbb0dbb24241
Sawashi <kietle234.cc@gmail.com>
2022-08-02 08:12:07 +0700

    update link web for server linking

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8888',
+    baseURL: 'https://cnpmapi.azurewebsites.net',
     // withCredentials: true
 });
 
