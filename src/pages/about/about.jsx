981cdc54dd560fd7d4519654f004fffd3d7bf2e4
Gobitech <thanh17082002@gmail.com>
2022-08-12 19:59:50 +0700

    About

---
+++ src/pages/about/about.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Button, Navbar, ListGroup, Dropdown, Carousel } from "react-bootstrap";
+import logo from "../../assets/images/logo.png";
+import welcome from "../../assets/images/welcome.png";
+import "./about.css";
+import { Link } from "react-router-dom";
+import setCookie from "../../hooks/setCookie";
+import getCookie from "../../hooks/getCookie";
+import removeCookie from "../../hooks/removeCookie";
+import NavbarWibu from '../../components/NavbarWibu';
+import FooterWibu from '../../components/FooterWibu';
